cdaa7f832b9497ce45edd604d65516720755e07a
Diego <diegombb1992@gmail.com>
2022-10-31 15:14:43 +0000

    SQL Terminado 3.1

---
+++ CrearTabla.sql
@@ -0,0 +1,12 @@
+update costumers 
+set email = 'ppicasso@gmail.com'
+where 'Pablo Picasso'=10001;
+
+update costumers 
+set email = 'lamarr_hd@hollywood.com'
+where 'hedy lamarr'=20001 ;
+
+update costumers 
+set email = 'hola@nasa.gov'
+where 'katherine johnson'=30001;
+
